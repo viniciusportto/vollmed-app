3dbedae17d484abeb9b59c8d2b0413abb75f8311
Vinicius Porto <viniciusportto@outlook.com>
2023-07-26 18:22:34 -0300

    mapping entities

---
+++ src/main/java/med/voll/api/doctor/Doctor.java
@@ -0,0 +1,30 @@
+package med.voll.api.doctor;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import med.voll.api.address.Address;
+
+@Table(name = "doctors")
+@Entity(name = "Doctors")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Doctor {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String crm;
+
+    @Enumerated(EnumType.STRING)
+    private String specialty;
+
+    @Embedded
+    private Address address;
+
+}
