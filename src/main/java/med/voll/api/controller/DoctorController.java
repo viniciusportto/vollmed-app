a8edda300904f1ffc0a62c956fa6d941f8074e02
Vinicius Porto <viniciusportto@outlook.com>
2023-08-09 18:09:58 -0300

    standardizing api returns

--- src/main/java/med/voll/api/controller/DoctorController.java
+++ src/main/java/med/voll/api/controller/DoctorController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,29 +20,35 @@ public class DoctorController {
 
     @PostMapping
     @Transactional
-    public void register(@RequestBody @Valid RegisterDoctorData datas){
+    public ResponseEntity register(@RequestBody @Valid RegisterDoctorData datas){
         repository.save(new Doctor(datas));
 
-    }
+        return ResponseEntity.noContent().build();
 
+    }
     //created get method and list of doctors
     @GetMapping
-    public Page<DataListingDoctor> lister(@PageableDefault(size = 10, page = 0, sort = {"name"}) Pageable pagination){
-        return repository.findAllByActiveTrue(pagination).map(DataListingDoctor::new);
+    public ResponseEntity <Page<DataListingDoctor>>lister(@PageableDefault(size = 10, page = 0, sort = {"name"}) Pageable pagination){
+        var page = repository.findAllByActiveTrue(pagination).map(DataListingDoctor::new);
+        return ResponseEntity.ok(page);
     }
 
     @PutMapping
     @Transactional
-    public void update(@RequestBody @Valid DataUpdateDoctor datas){
+    public ResponseEntity update(@RequestBody @Valid DataUpdateDoctor datas){
         var doctor = repository.getReferenceById(datas.id());
         doctor.updateInformations(datas);
+
+        return ResponseEntity.ok(new datasDetailingDoctor(doctor));
     }
 
     @DeleteMapping("/{id}")
     @Transactional
-    public void delete(@PathVariable Long id){
+    public ResponseEntity delete(@PathVariable Long id){
         var doctor = repository.getReferenceById(id);
         doctor.delete();
+
+        return ResponseEntity.noContent().build();
     }
 
 
---
+++ src/main/java/med/voll/api/doctor/datasDetailingDoctor.java
@@ -0,0 +1,11 @@
+package med.voll.api.doctor;
+
+import com.sun.jdi.event.StepEvent;
+import med.voll.api.address.Address;
+
+public record datasDetailingDoctor(Long id, String name, String email, String crm, String phone, Specialty specialty, Address address) {
+
+    public  datasDetailingDoctor(Doctor doctor){
+        this(doctor.getId(), doctor.getName(), doctor.getEmail(), doctor.getCrm(), doctor.getPhone(), doctor.getSpecialty(), doctor.getAddress());
+    }
+}
