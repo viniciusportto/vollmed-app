ed69476ed18515d0503324ba855c607360d33aca
Vinicius Porto <viniciusportto@outlook.com>
2023-08-24 18:07:59 -0300

    autentication Controller

---
+++ src/main/java/med/voll/api/controller/AutenticationController.java
@@ -0,0 +1,28 @@
+package med.voll.api.controller;
+
+import jakarta.validation.Valid;
+import med.voll.api.domain.user.DatasAutentication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
+public class AutenticationController {
+
+    @Autowired
+    public AuthenticationManager manager;
+
+    @PostMapping()
+    public ResponseEntity login(@RequestBody @Valid DatasAutentication datas){
+        var token = new UsernamePasswordAuthenticationToken(datas.login(), datas.password());
+        var authentication = manager.authenticate(token);
+
+        return ResponseEntity.ok().build();
+    }
+}
---
+++ src/main/java/med/voll/api/domain/user/DatasAutentication.java
@@ -0,0 +1,4 @@
+package med.voll.api.domain.user;
+
+public record DatasAutentication(String login, String password) {
+}
