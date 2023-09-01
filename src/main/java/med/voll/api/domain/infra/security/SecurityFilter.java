b2dc146bcffdb4c001a4bfc6e8b5cfdc4e38ef76
Vinicius Porto <viniciusportto@outlook.com>
2023-09-01 20:01:03 -0300

    retrieving token

--- src/main/java/med/voll/api/domain/infra/security/SecurityFilter.java
+++ src/main/java/med/voll/api/domain/infra/security/SecurityFilter.java
@@ -14,8 +14,19 @@ public class SecurityFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var tokenJWT = retrieveToken(request);
+
+        System.out.println(tokenJWT);
 
         filterChain.doFilter(request, response);
 
+    }
+
+    private String retrieveToken(HttpServletRequest request) {
+        var authorizationHeader = request.getHeader("Authorization");
+        if(authorizationHeader == null){
+            throw new RuntimeException("JWT token not sent in Authorization header!");
+        }
+        return authorizationHeader.replace("Bearer ", "");
     }
 }
