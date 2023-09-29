8d9d6f94c0dae3acddfefe46d0572649674f0dfa
Vinicius Porto <viniciusportto@outlook.com>
2023-09-29 16:37:35 -0300

    init create tests

--- src/main/java/med/voll/api/infra/springdoc/SpringDocConfigurations.java
+++ src/main/java/med/voll/api/infra/springdoc/SpringDocConfigurations.java
@@ -1,7 +1,10 @@
 package med.voll.api.infra.springdoc;
 
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +17,15 @@ public class SpringDocConfigurations {
         return new OpenAPI()
                 .components(new Components()
                         .addSecuritySchemes("bearer-key",
-                                new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")));
+                                new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")))
+                .info(new Info()
+                        .title("Voll.med API")
+                        .description("Rest API of the Voll.med application, containing CRUD functionalities for doctors and patients, as well as scheduling and canceling appointments")
+                        .contact(new Contact()
+                                .name("Backend Team")
+                                .email("viniciusportto@outlook.com"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("http://voll.med/api/licence")));
     }
 }
